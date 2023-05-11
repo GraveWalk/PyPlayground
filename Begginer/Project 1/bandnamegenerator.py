9c1715fd674a21a26ebbe8c6c31134150c688df4
Kushagra <113252157+GraveWalk@users.noreply.github.com>
2023-05-11 13:05:33 +0530

    Update bandnamegenerator.py

--- Begginer/Project 1/bandnamegenerator.py
+++ Begginer/Project 1/bandnamegenerator.py
@@ -1,1 +1,16 @@
+# This is a simple Project for a Name Generator you see i have seen many examples like this (Band Name Generator),
+#so if anyone thinks i copied don't mind but yes sir i did
+# Let's start
 
+#Let's start by some greeting
+print("Hello! Let's just cut to chase and make a good name for your band name: ")
+
+# Well my idea is to use country name or city name + pet name
+# we will require 2 variable 1 for city and 2 for pet name
+
+city = input("Enter your city name: ")
+pet_name = input("Enter your pet name: ")
+band_name = city + pet_name
+#Now we will simply print this
+
+print("Here is a suggestion "+band_name)
